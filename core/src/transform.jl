8b6e3c6aeec9752352cc2743434e6f20032751eb
Bart de Koning <bart123-4@hotmail.com>
2023-12-05 12:35:24 +0100

    Getting_started_shapes runs without errors

--- core/src/transform.jl
+++ core/src/transform.jl
@@ -1,55 +1,55 @@
-abstract type RayTransform{F<:AbstractFloat} end
+abstract type RayTransform{F <: AbstractFloat} end
 
 """
 A ray transform type that can contain a scaling, a rotation
 and a translation.
 """
 struct AffineTransform{
-    F<:AbstractFloat,
-    S<:Union{F,Missing},
-    R<:Union{Matrix{F},Missing},
-    T<:Union{Vector{F},Missing},
+	F <: AbstractFloat,
+	S <: Union{F, Missing},
+	R <: Union{Matrix{F}, Missing},
+	T <: Union{Vector{F}, Missing},
 } <: RayTransform{F}
-    scaling::S
-    rotation::R
-    rotation_inverse::R
-    translation::T
+	scaling::S
+	rotation::R
+	rotation_inverse::R
+	translation::T
 end
 
 function Base.show(io::IO, transform::AffineTransform{F})::Nothing where {F}
-    (; scaling, rotation, translation) = transform
-    if transform isa AffineTransform{F,Missing,Missing,Missing}
-        print(io, "<AffineTransform; identity>")
-    else
-        s = "<AffineTransform;"
-        print(io, "<AffineTransform; with ")
-        if !ismissing(scaling)
-            s *= "scaling, "
-        end
-        if !ismissing(rotation)
-            s *= "rotation, "
-        end
-        if !ismissing(translation)
-            s *= "translation, "
-        end
-        s = s[1:end-2] * ">"
-        print(io, s)
-    end
-    return nothing
+	(; scaling, rotation, translation) = transform
+	if transform isa AffineTransform{F, Missing, Missing, Missing}
+		print(io, "<AffineTransform; identity>")
+	else
+		s = "<AffineTransform;"
+		print(io, "<AffineTransform; with ")
+		if !ismissing(scaling)
+			s *= "scaling, "
+		end
+		if !ismissing(rotation)
+			s *= "rotation, "
+		end
+		if !ismissing(translation)
+			s *= "translation, "
+		end
+		s = s[1:end-2] * ">"
+		print(io, s)
+	end
+	return nothing
 end
 
 """
 Get the identity (affine) transform of given type.
 """
-function identity_transform(F)::AffineTransform{F,Missing,Missing,Missing}
-    return AffineTransform{F,Missing,Missing,Missing}(missing, missing, missing, missing)
+function identity_transform(F)::AffineTransform{F, Missing, Missing, Missing}
+	return AffineTransform{F, Missing, Missing, Missing}(missing, missing, missing, missing)
 end
 
 """
 Get an affine transform which only consists of a translation.
 """
 function translation(vector::Vector{F})::AffineTransform{F} where {F}
-    return AffineTransform(missing, missing, missing, vector)
+	return AffineTransform(missing, missing, missing, vector)
 end
 
 """
@@ -58,14 +58,14 @@ axis by the given amount.
 Note: The axis must be a unit vector.
 """
 function rotation(axis::Vector{F}, θ::F)::AffineTransform{F} where {F}
-    R = zeros(F, 3, 3)
-    W = [0 -axis[3] axis[2]; axis[3] 0 -axis[1]; -axis[2] axis[1] 0]
-    for i ∈ 1:3
-        R[i, i] += 1
-    end
-    R += sin(θ) * W
-    R += (2 * sin(θ / 2)^2) * W^2
-    return AffineTransform(missing, R, inv(R), missing)
+	R = zeros(F, 3, 3)
+	W = [0 -axis[3] axis[2]; axis[3] 0 -axis[1]; -axis[2] axis[1] 0]
+	for i ∈ 1:3
+		R[i, i] += 1
+	end
+	R += sin(θ) * W
+	R += (2 * sin(θ / 2)^2) * W^2
+	return AffineTransform(missing, R, inv(R), missing)
 end
 
 """
@@ -73,35 +73,35 @@ Define the * operator for composing (combining)
 2 affine transformations.
 """
 function Base.:*(
-    transform_second::AffineTransform{F},
-    transform_first::AffineTransform{F},
+	transform_second::AffineTransform{F},
+	transform_first::AffineTransform{F},
 )::AffineTransform{F} where {F}
-    scaling_first = coalesce(transform_first.scaling, one(F))
-    scaling_second = coalesce(transform_second.scaling, one(F))
-    rotation_first = coalesce(transform_first.rotation, identity_matrix(F))
-    rotation_second = coalesce(transform_second.rotation, identity_matrix(F))
-    translation_first = coalesce(transform_first.translation, zeros(F, 3))
-    translation_second = coalesce(transform_second.translation, zeros(F, 3))
+	scaling_first = coalesce(transform_first.scaling, one(F))
+	scaling_second = coalesce(transform_second.scaling, one(F))
+	rotation_first = coalesce(transform_first.rotation, identity_matrix(F))
+	rotation_second = coalesce(transform_second.rotation, identity_matrix(F))
+	translation_first = coalesce(transform_first.translation, zeros(F, 3))
+	translation_second = coalesce(transform_second.translation, zeros(F, 3))
 
-    scaling = scaling_first * scaling_second
+	scaling = scaling_first * scaling_second
 
-    rotation = scaling * rotation_second * rotation_first
-    if all(rotation ≈ identity_matrix(F))
-        rotation = missing
-        rotation_inverse = missing
-    else
-        rotation_inverse = inv(rotation)
-    end
+	rotation = scaling * rotation_second * rotation_first
+	if all(rotation ≈ identity_matrix(F))
+		rotation = missing
+		rotation_inverse = missing
+	else
+		rotation_inverse = inv(rotation)
+	end
 
-    translation = scaling_second * rotation_second * translation_first + translation_second
-    if all(translation .≈ zero(F))
-        translation = missing
-    end
+	translation = scaling_second * rotation_second * translation_first + translation_second
+	if all(translation .≈ zero(F))
+		translation = missing
+	end
 
-    if scaling ≈ one(F)
-        scaling = missing
-    end
-    return AffineTransform(scaling, rotation, rotation_inverse, translation)
+	if scaling ≈ one(F)
+		scaling = missing
+	end
+	return AffineTransform(scaling, rotation, rotation_inverse, translation)
 end
 
 """
@@ -109,34 +109,34 @@ Apply an affine transformation on the source ray and store the result
 in the destination ray.
 """
 function forward_transform!(
-    loc_dst::VF,
-    dir_dst::VF,
-    loc_src::VF,
-    dir_src::VF,
-    vec_temp::AbstractVector{F},
-    transform::AffineTransform{F},
-)::Nothing where {F,VF}
-    (; scaling, rotation, translation) = transform
-    if loc_src != loc_dst
-        copyto!(loc_dst, loc_src)
-        copyto!(dir_dst, dir_src)
-    end
+	loc_dst::VF,
+	dir_dst::VF,
+	loc_src::VF,
+	dir_src::VF,
+	vec_temp::AbstractVector{F},
+	transform::AffineTransform{F},
+)::Nothing where {F, VF}
+	(; scaling, rotation, translation) = transform
+	if loc_src != loc_dst
+		copyto!(loc_dst, loc_src)
+		copyto!(dir_dst, dir_src)
+	end
 
-    if !ismissing(rotation)
-        mul!(vec_temp, rotation, loc_dst)
-        copyto!(loc_dst, temp_vector)
-        mul!(vec_temp, rotation, dir_dst)
-        copyto!(dir_dst, temp_vector)
-    end
-    if !ismissing(scaling)
-        for i ∈ 1:3
-            loc_dst[i] *= scaling
-        end
-    end
-    if !ismissing(translation)
-        loc_dst .+= view(translation, :)
-    end
-    return nothing
+	if !ismissing(rotation)
+		mul!(vec_temp, rotation, loc_dst)
+		copyto!(loc_dst, temp_vector)
+		mul!(vec_temp, rotation, dir_dst)
+		copyto!(dir_dst, temp_vector)
+	end
+	if !ismissing(scaling)
+		for i ∈ 1:3
+			loc_dst[i] *= scaling
+		end
+	end
+	if !ismissing(translation)
+		loc_dst .+= view(translation, :)
+	end
+	return nothing
 end
 
 """
@@ -144,30 +144,30 @@ Apply the inverse of an affine transform on the source ray and store
 the results in the destination ray.
 """
 function inverse_transform!(
-    loc_dst::VF,
-    dir_dst::VF,
-    loc_src::VF,
-    dir_src::VF,
-    vec_temp::AbstractVector{F},
-    transform::AffineTransform{F},
-)::Nothing where {F,VF}
-    (; scaling, rotation_inverse, translation) = transform
-    if loc_src !== loc_dst
-        copyto!(loc_dst, loc_src)
-        copyto!(dir_dst, dir_src)
-    end
+	loc_dst::VF,
+	dir_dst::VF,
+	loc_src::VF,
+	dir_src::VF,
+	vec_temp::AbstractVector{F},
+	transform::AffineTransform{F},
+)::Nothing where {F, VF}
+	(; scaling, rotation_inverse, translation) = transform
+	if loc_src !== loc_dst
+		copyto!(loc_dst, loc_src)
+		copyto!(dir_dst, dir_src)
+	end
 
-    if !ismissing(translation)
-        loc_dst .-= view(translation, :)
-    end
-    if !ismissing(scaling)
-        loc_dst ./= scaling
-    end
-    if !ismissing(rotation_inverse)
-        mul!(vec_temp, rotation_inverse, loc_dst)
-        copyto!(loc_dst, vec_temp)
-        mul!(vec_temp, rotation_inverse, ray_dst.dir)
-        copyto!(dir_dst, vec_temp)
-    end
-    return nothing
+	if !ismissing(translation)
+		loc_dst .-= view(translation, :)
+	end
+	if !ismissing(scaling)
+		loc_dst ./= scaling
+	end
+	if !ismissing(rotation_inverse)
+		mul!(vec_temp, rotation_inverse, loc_dst)
+		copyto!(loc_dst, vec_temp)
+		mul!(vec_temp, rotation_inverse, dir_dst)
+		copyto!(dir_dst, vec_temp)
+	end
+	return nothing
 end
--- core/src/utils.jl
+++ core/src/utils.jl
@@ -1,64 +1,64 @@
 const TypedSubArray = SubArray{
-    T,
-    1,
-    MT,
-    Tuple{Int64,Base.Slice{Base.OneTo{Int64}}},
-    true,
-} where {T,MT<:AbstractMatrix{T}}
+	T,
+	1,
+	MT,
+	Tuple{Int64, ST},
+	true,
+} where {T, MT <: AbstractMatrix{T}, ST}
 const Intersector = FunctionWrapper{
-    Nothing,
-    Tuple{VFS,VFS,VFS,VFS,VFS,VIS,VFS,TypedSubArray{Symbol,Matrix{Symbol}}},
+	Nothing,
+	Tuple{VFS, VFS, VFS, VFS, VFS, VIS, VFS, TypedSubArray{Symbol, Matrix{Symbol}}},
 } where {
-    VFS<:TypedSubArray{F,MF} where {F<:AbstractFloat,MF<:AbstractMatrix{F}},
-    VIS<:TypedSubArray{Int,MI} where {MI<:AbstractMatrix{Int}},
+	VFS <: TypedSubArray{F, MF, Base.Slice{Base.OneTo{Int64}}} where {F <: AbstractFloat, MF <: AbstractMatrix{F}},
+	VIS <: TypedSubArray{Int, MI, Base.Slice{Base.OneTo{Int64}}} where {MI <: AbstractMatrix{Int}},
 }
 const Texturer = FunctionWrapper{
-    Nothing,
-    Tuple{VFS,VIS,VFS},
+	Nothing,
+	Tuple{VFS, VIS, VFS},
 } where {
-    VFS<:TypedSubArray{F,MF} where {F<:AbstractFloat,MF<:AbstractMatrix{F}},
-    VIS<:TypedSubArray{Int,MI} where {MI<:AbstractMatrix{Int}},
+	VFS <: TypedSubArray{F, MF, Base.Slice{Base.OneTo{Int64}}} where {F <: AbstractFloat, MF <: AbstractMatrix{F}},
+	VIS <: TypedSubArray{Int, MI, Base.Slice{Base.OneTo{Int64}}} where {MI <: AbstractMatrix{Int}},
 }
 
-const ScalarFunc = FunctionWrapper{F,Tuple{F}} where {F<:AbstractFloat}
+const ScalarFunc = FunctionWrapper{F, Tuple{F}} where {F <: AbstractFloat}
 const Transform = FunctionWrapper{
-    Nothing,
-    Tuple{TypedSubArray{F,MF}},
-} where {F<:AbstractFloat,MF<:AbstractMatrix{F}}
+	Nothing,
+	Tuple{TypedSubArray{F, MF, ST}},
+} where {F <: AbstractFloat, MF <: AbstractMatrix{F}, ST}
 const ScalarField = FunctionWrapper{
-    F,
-    Tuple{TypedSubArray{F,MF}},
-} where {F<:AbstractFloat,MF<:AbstractMatrix{F}}
+	F,
+	Tuple{TypedSubArray{F, MF, ST}},
+} where {ST, F <: AbstractFloat, MF <: AbstractMatrix{F}}
 const VectorField =
-    FunctionWrapper{Nothing,Tuple{Vector{F},Vector{F}}} where {F<:AbstractFloat}
+	FunctionWrapper{Nothing, Tuple{VF, VF}} where {F <: AbstractFloat, VF <: AbstractVector{F}}
 
-mutable struct Dtimer{F<:AbstractFloat}
-    t::F
+mutable struct Dtimer{F <: AbstractFloat}
+	t::F
 end
 
 function Base.convert(::Type{Dtimer{F}}, dtimer::Dtimer) where {F}
-    return Dtimer(convert(F, dtimer.t))
+	return Dtimer(convert(F, dtimer.t))
 end
 
 function start!(dtimer::Dtimer)::Nothing
-    dtimer.t = time_ns() / 1e9
-    return nothing
+	dtimer.t = time_ns() / 1e9
+	return nothing
 end
 
 function get_Δt!(dtimer::Dtimer{F})::F where {F}
-    t = time_ns() / 1e9
-    Δt = t - dtimer.t
-    dtimer.t = t
-    return Δt
+	t = time_ns() / 1e9
+	Δt = t - dtimer.t
+	dtimer.t = t
+	return Δt
 end
 
 function snake_case_name(T)::Symbol
-    name = String(Base.typename(T).name)
-    parts = eachmatch(r"[A-Z][a-z]*", name)
-    name_snake_case = join([part.match for part in parts], "_") |> lowercase
-    return Symbol(name_snake_case)
+	name = String(Base.typename(T).name)
+	parts = eachmatch(r"[A-Z][a-z]*", name)
+	name_snake_case = join([part.match for part in parts], "_") |> lowercase
+	return Symbol(name_snake_case)
 end
 
 function identity_matrix(F)::Matrix
-    return F[1 0 0; 0 1 0; 0 0 1]
+	return F[1 0 0; 0 1 0; 0 0 1]
 end
