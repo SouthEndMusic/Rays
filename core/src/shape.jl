ae0d563f07d6f36d2a44357c47e5c2ca0661151d
Bart de Koning <bart123-4@hotmail.com>
2024-01-01 16:33:04 +0100

    Fix tetrahedron normals

--- core/src/shape.jl
+++ core/src/shape.jl
@@ -156,7 +156,7 @@ function Tetrahedron(R::F)::TriangleShape{F} where {F}
         ]
     end
 
-    faces = [1 2 3; 1 2 4; 1 3 4; 2 3 4]
+    faces = [1 3 2; 1 2 4; 1 4 3; 2 3 4]
 
     return TriangleShape(vertices, faces; convex = true, name = :tetrahedron)
 end
